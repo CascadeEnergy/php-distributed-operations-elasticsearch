cc7ec071d4ff1d383d1a6c5b1701f87b14822bb2
Charles Berube <charles.berube@cascadeenergy.com>
2015-10-29 15:46:34 -0700

    isBatchDone implementation for Waiter

--- src/Waiter.php
+++ src/Waiter.php
@@ -3,6 +3,7 @@
 namespace CascadeEnergy\DistributedOperations\Elasticsearch;
 
 use CascadeEnergy\DistributedOperations\Operation;
+use CascadeEnergy\DistributedOperations\Utility\CounterInterface;
 use CascadeEnergy\DistributedOperations\Utility\UtilityFactoryConsumerInterface;
 use CascadeEnergy\DistributedOperations\Utility\UtilityFactoryConsumerTrait;
 use CascadeEnergy\DistributedOperations\Utility\WaiterInterface;
@@ -16,6 +17,9 @@ class Waiter implements WaiterInterface, UtilityFactoryConsumerInterface
     /** @var int */
     private $delayTime = self::DEFAULT_DELAY_TIME;
 
+    /** @var CounterInterface[] */
+    private $counterCache = [];
+
     /**
      * @param int $delayTime The delay time, in seconds
      */
@@ -24,14 +28,44 @@ class Waiter implements WaiterInterface, UtilityFactoryConsumerInterface
         $this->delayTime = $delayTime;
     }
 
+    public function isBatchDone($batchId)
+    {
+        $counter = $this->getCounter($batchId);
+
+        if ($counter->getCount() == 0) {
+            $this->deleteCounter($batchId);
+            return true;
+        }
+
+        return false;
+    }
+
     public function waitForBatch($batchId)
     {
-        $counter = $this->utilityFactory->createCounter();
-        $counter->setBatchId($batchId);
-        $counter->setState(Operation::STATE_NEW);
+        $counter = $this->getCounter($batchId);
 
         do {
             sleep($this->delayTime);
         } while ($counter->getCount() > 0);
+
+        $this->deleteCounter($batchId);
+    }
+    
+    private function getCounter($batchId)
+    {
+        if (!array_key_exists($batchId, $this->counterCache)) {
+            $counter = $this->utilityFactory->createCounter();
+            $counter->setBatchId($batchId);
+            $counter->setState(Operation::STATE_NEW);
+            
+            $this->counterCache[$batchId] = $counter;
+        }
+        
+        return $this->counterCache[$batchId];
+    }
+
+    private function deleteCounter($batchId)
+    {
+        unset($this->counterCache[$batchId]);
     }
 }
